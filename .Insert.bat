111c4f58a3c624be052eeedfacb04133357e7a04
Greenphx9 <55181320+Greenphx9@users.noreply.github.com>
2022-09-13 21:16:42 +1000

    Update some dex entries, fix last 2 dex

--- .Insert.bat
+++ .Insert.bat
@@ -1,3 +1,9 @@
 py -3.7 scripts//make.py
 del D:\Modding\Complete-Fire-Red-Upgrade\BPRE0.gba
-pause
+del D:\Modding\Complete-Fire-Red-Upgrade\test.gba
+copy D:\Modding\Pokem.onV2\Dynamic-Pokemon-Expansion\test.gba D:\Modding\Complete-Fire-Red-Upgrade\BPRE0.gba
+cd ..
+cd ..
+cd Complete-Fire-Red-Upgrade
+.Insert.bat
+pause 
